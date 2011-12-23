db5fc1e5716d053b643cc9fa43ca3065554db9f6
Joshua Thijssen <jthijssen@noxlogic.nl>
2011-12-23 17:04:09 +0100

    BuildFromIterator does not work correctly. Made it a bit simpler

--- phar/buildphar.php
+++ phar/buildphar.php
@@ -4,44 +4,9 @@
 @unlink('htrouter.phar');
 $phar = new Phar('htrouter.phar', 0, 'htrouter.phar');
 
-
 $basePath = realpath(dirname(__FILE__)."/..");
-
-// Create iterator
-$it = new RecursiveDirectoryIterator($basePath);
-$it = new UnwantedRecursiveFilterIterator($it, array(".idea", ".git"));
-$it = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::SELF_FIRST);
-$phar->buildFromIterator($it, realpath($basePath."/.."));
+$phar->buildFromDirectory($basePath, '/\.php$/');
 
 // Add stub
-$phar->setStub($phar->createDefaultStub('router/public/router.php', 'router/public/router.php'));
+$phar->setStub($phar->createDefaultStub('/public/router.php', '/public/router.php'));
 
-
-/**
- * Simple filter that will skip a serie of directories
- */
-class UnwantedRecursiveFilterIterator extends RecursiveFilterIterator {
-    protected $_excludedPatterns = array();
-
-    public function __construct(RecursiveIterator $it, array $excludedPatterns)
-    {
-        $this->_excludedPatterns = $excludedPatterns;
-        parent::__construct($it);
-    }
-
-    public function accept()
-    {
-        foreach ($this->_excludedPatterns as $pattern) {
-            if (fnmatch($pattern, $this->current()->getBasename())) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    function getChildren()
-    {
-        $it = new RecursiveDirectoryIterator($this->current());
-        return new UnwantedRecursiveFilterIterator($it, $this->_excludedPatterns);
-    }
-}
