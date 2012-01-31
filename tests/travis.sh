3a13d4d3083e1d6cc87601bee6f5f487365d3a5f
Joshua Thijssen <jthijssen@noxlogic.nl>
2012-01-31 18:28:47 +0100

    Update on travis

---
+++ tests/travis.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+
+cd tests
+
+phpunit \
+    --configuration phpunit.xml \
+    --verbose \
+    --stop-on-failure \
+    --colors \
+    --bootstrap phpunit-boot.php \
+    -d memory_limit=-1 \
+    -d display_startup_errors=0 \
+    $dir
+
